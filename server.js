62fca7745175a32300cc4e6b4c80983076b80c4d
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-08-23 09:52:16 -0700

    connect to mongoDB

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+require('./config/database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
