af1ef7d44200776b5b10910fd35a335a7af380c7
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-08-25 04:05:58 -0700

    add usa news api

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,21 @@ var request = require('request');
 
 const apiKey = '9d290332d83746ee974d4e50f4ba29e6';
 const ukSportApi = `https://newsapi.org/v2/top-headlines?country=gb&category=sports&apiKey=${apiKey}`;
+const usaSportApi = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${apiKey}`;
 
 /* GET home page. */
 router.get('/uk', function(req, res, next) {
   request.get(ukSportApi, (error, response, newsData)=>{
     const parsedData = JSON.parse(newsData);
     res.render('api/uk', {
+      newsData: parsedData.articles
+    })
+  })
+});
+router.get('/usa', function(req, res, next) {
+  request.get(usaSportApi, (error, response, newsData)=>{
+    const parsedData = JSON.parse(newsData);
+    res.render('api/usa', {
       newsData: parsedData.articles
     })
   })
