a7d5dc13b377675b66605d8f27320200aff3f240
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-08-29 16:39:57 -0700

    format views

--- routes/api.js
+++ routes/api.js
@@ -6,21 +6,21 @@ const apiKey = '9d290332d83746ee974d4e50f4ba29e6';
 const ukSportApi = `https://newsapi.org/v2/top-headlines?country=gb&category=sports&apiKey=${apiKey}`;
 const usaSportApi = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${apiKey}`;
 
-router.get('/uk', function(req, res, next) {
-  request.get(ukSportApi, (error, response, newsData)=>{
-    const parsedData = JSON.parse(newsData);
-    res.render('api/uk', {
-      newsData: parsedData.articles
+router.get('/uk', function (req, res, next) {
+    request.get(ukSportApi, (error, response, newsData) => {
+        const parsedData = JSON.parse(newsData);
+        res.render('api/uk', {
+            newsData: parsedData.articles
+        })
     })
-  })
 });
-router.get('/usa', function(req, res, next) {
-  request.get(usaSportApi, (error, response, newsData)=>{
-    const parsedData = JSON.parse(newsData);
-    res.render('api/uk', {
-      newsData: parsedData.articles
+router.get('/usa', function (req, res, next) {
+    request.get(usaSportApi, (error, response, newsData) => {
+        const parsedData = JSON.parse(newsData);
+        res.render('api/uk', {
+            newsData: parsedData.articles
+        })
     })
-  })
 });
 
 module.exports = router;
